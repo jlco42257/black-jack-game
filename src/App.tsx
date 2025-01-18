17789db73728942033f8b26d108d076e673c79a4
jlco42257 <jlco42257@gmail.com>
2025-01-17 21:30:28 -0500

    fix in deck initializacion, add styles and confeti

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Card from "./components/Card";
 import { useState } from "react";
-import { CardData } from "./types";
+import { CardData, DeckCard } from "./types";
 import { DECK, GAME_STATUS, RANKS } from "./constants";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
 
 function App() {
   const [gameStatus, setGameStatus] = useState(GAME_STATUS.READY);
@@ -10,7 +12,8 @@ function App() {
   const [playerScore, setPlayerScore] = useState(0);
   const [dealerCards, setDealerCards] = useState<CardData[]>([]);
   const [dealerScore, setDealerScore] = useState(0);
-  const [deckGame, setDeckGame] = useState(DECK);
+  const [deckGame, setDeckGame] = useState([...DECK]);
+  const { width, height } = useWindowSize();
 
   function calculateScore(cards: CardData[]): number {
     const sum = cards.reduce((total, card) => total + card.value!, 0);
@@ -22,9 +25,9 @@ function App() {
     return sum;
   }
 
-  function generateCard() {
-    const newDeck = deckGame;
-    const randomCard = deckGame[Math.floor(Math.random() * deckGame.length)];
+  function generateCard(deck: DeckCard[]) {
+    const newDeck = deck;
+    const randomCard = deck[Math.floor(Math.random() * deck.length)];
     const indexToDelete = newDeck.indexOf(randomCard);
 
     newDeck.splice(indexToDelete, 1);
@@ -36,55 +39,63 @@ function App() {
       value: RANKS[randomCard.rank as keyof typeof RANKS],
     };
     return card;
-  };
-
-  function initGame(){
-    setDeckGame(DECK);
-    const newPlayerHand = [generateCard(),generateCard()]
-    const newDealerHand = [generateCard()]
-
-    setPlayerCards(newPlayerHand)
-    setDealerCards(newDealerHand)
-    setPlayerScore(calculateScore(newPlayerHand))
-    setDealerScore(calculateScore(newDealerHand))
-    setGameStatus(GAME_STATUS.PLAYING)
-  };
-  function hitCard() {
-    const newHand = [...playerCards, generateCard()]
-    const newScore = calculateScore(newHand)
-
-    setPlayerCards(newHand)
-    setPlayerScore(newScore)
-    if(newScore > 21) setGameStatus(GAME_STATUS.LOSE)
   }
 
-  function stand() {
+  // Función para crear la pausa
+  const pause = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+   function initGame() {
+    const newDeck = Array.from(DECK);
+    setDeckGame(newDeck);
+    const newPlayerHand = [generateCard(newDeck), generateCard(newDeck)];
+    const newDealerHand = [generateCard(newDeck)];
+
+    setPlayerCards(newPlayerHand);
+    setDealerCards(newDealerHand);
+    setPlayerScore(calculateScore(newPlayerHand));
+    setDealerScore(calculateScore(newDealerHand));
+    setGameStatus(GAME_STATUS.PLAYING);
+  }
+  function hitCard() {
+    const newDeck = deckGame
+    const newHand = [...playerCards, generateCard(newDeck)];
+    const newScore = calculateScore(newHand);
+
+    setPlayerCards(newHand);
+    setPlayerScore(newScore);
+    if (newScore > 21) setGameStatus(GAME_STATUS.LOSE);
+  }
+
+  async function stand() {
     setGameStatus(GAME_STATUS.DEALER_TURN);
-    let currentScore = dealerScore
-    let currentHand = dealerCards
+    let currentScore = dealerScore;
+    let currentHand = dealerCards;
 
     while (currentScore <= 16) {
-      currentHand = [...currentHand, generateCard()]
-      setDealerCards(currentHand)
-      currentScore = calculateScore(currentHand)
-      setDealerScore(currentScore)
+      const newDeck = deckGame
+      await pause(1500);
+      currentHand = [...currentHand, generateCard(newDeck)];
+      setDealerCards(currentHand);
+      currentScore = calculateScore(currentHand);
+      setDealerScore(currentScore);
     }
 
+    await pause(1500)
     if (currentScore >= playerScore && currentScore < 22) {
-      setGameStatus(GAME_STATUS.LOSE)
-    }else{
-      setGameStatus(GAME_STATUS.WIN)
+      setGameStatus(GAME_STATUS.LOSE);
+    } else {
+      setGameStatus(GAME_STATUS.WIN);
     }
   }
   return (
-    <>
-      <h1>Black Jack Game</h1>
+    <main className="game__container">
+      <h1 className="brand">Black Jack Game</h1>
       {gameStatus == GAME_STATUS.READY && (
         <button onClick={initGame} className="play-button">
           play
         </button>
       )}
-      {gameStatus != GAME_STATUS.READY && <h2>Dealer Hand</h2>}
       <div className="player-cards__container">
         {gameStatus != GAME_STATUS.READY &&
           dealerCards.map((card, index) => (
@@ -95,11 +106,10 @@ function App() {
           ))}
       </div>
       {gameStatus != GAME_STATUS.READY && (
-        <div className="score">
-          <p> Dealer score:{dealerScore}</p>
-        </div>
+        <>
+          <p className="score">score:{dealerScore}</p>
+        </>
       )}
-      {gameStatus != GAME_STATUS.READY && <h2>Your Hand</h2>}
       <div className="player-cards__container">
         {gameStatus != GAME_STATUS.READY &&
           playerCards.map((card, index) => (
@@ -110,9 +120,7 @@ function App() {
           ))}
       </div>
       {gameStatus != GAME_STATUS.READY && (
-        <div className="score">
-          <p>score:{playerScore}</p>
-        </div>
+        <p className="score">score:{playerScore}</p>
       )}
       {gameStatus == GAME_STATUS.PLAYING && (
         <div className="actions__container">
@@ -121,7 +129,7 @@ function App() {
         </div>
       )}
       {gameStatus == GAME_STATUS.LOSE && (
-        <div className="game-over__container">
+        <div className="game-finish__container">
           <h2>You Lose!</h2>
           <button onClick={initGame} className="new-game__button">
             new game
@@ -129,14 +137,15 @@ function App() {
         </div>
       )}
       {gameStatus == GAME_STATUS.WIN && (
-        <div className="game-over__container">
+        <div className="game-finish__container">
           <h2>You Win!</h2>
           <button onClick={initGame} className="new-game__button">
             new game
           </button>
+          <Confetti width={width - 15} height={height - 15} />
         </div>
       )}
-    </>
+    </main>
   );
 }
 
